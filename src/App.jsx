5c12185d61f9932d3200d159250e6f74f92e3b8d
Shah-Abdullah-engr <hashmiabdullah788@gmail.com>
2026-01-09 22:17:33 +0500

    prop

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import Student from "./student";
+ 
 
-  return (
+function App(){
+  return(
     <>
-      Hello world
+  <Student name="Shah Abdullah" class="12th" roll_no={339}/>
+    
     </>
-  )
-}
+  );
 
+}
 export default App
+
---
+++ src/student.jsx
@@ -0,0 +1,12 @@
+function Student(props){
+  return(
+    <div>
+      <p>Name : {props.name}</p>
+      <p>Class : {props.class}</p>
+      <p>Roll No : {props.roll_no}</p>
+    </div>
+  );
+
+
+} 
+export default Student 
